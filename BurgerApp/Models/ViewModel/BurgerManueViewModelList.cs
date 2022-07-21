4c6bb4d5325892f15b98b475e6783bae404e2731
Dinko <98716869+DinelHasic@users.noreply.github.com>
2022-07-21 19:07:27 +0200

    Delete BurgerApp/Models directory

--- BurgerApp/Models/ViewModel/BurgerManueViewModelList.cs
+++
@@ -1,9 +0,0 @@
-﻿using BurgerApp.Models.Domain;
-
-namespace BurgerApp.Models.ViewModel
-{
-    public class BurgerManueViewModelList
-    {
-        public List<BurgerMenuViewModel> Burgers { get; set; }
-    }
-}
